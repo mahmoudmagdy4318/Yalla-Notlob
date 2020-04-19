e6d18ac4fc04c3065f9f79c9e8f0cbb9c950ca59
FatmaSaad <fatma_s3@yahoo.com>
2020-04-19 21:13:57 +0200

    conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-Rails.application.routes.draw do
-  get 'home/index'
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :friends
-  root to: "home#index"
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-=======
 Rails.application.routes.draw do
   get 'home/index'
   
@@ -19,4 +8,3 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
->>>>>>> 8cd25da9a46cee863821ae5f67b6753bf01a29a1
