1ff81a84443033f76ba26cfc005ff93af7f1e6a7
mahmoud magdy <mahmoudmagdy4318@gmail.com>
2020-04-20 02:03:36 +0200

    orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
 
+  resources :orders
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
