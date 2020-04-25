0442456aa1febceadd3e4ed804f1d197bc1687dd
minariaad <mina.riaad@gmail.com>
2020-04-25 19:39:16 +0200

    dgdfgfd

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -7,6 +7,10 @@ class OrderDetailsController < ApplicationController
         @orderDetails= OrderDetail.create(order_id: params[:order_id] ,user_id: current_user.id,item: params[:item],
              amount: params[:amount],price: params[:price],comment: params[:comment])
         if @orderDetails.save
+
+            @notification=Notification.create(body: params[:item] + "is recieved",user_id: 1)
+            ActionCable.server.broadcast 'notification_channel', content: @notification
+
             redirect_to request.referrer
         else
             render 'new'
